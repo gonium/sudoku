e7b48f0aaabf2d0e2d4c60a957e9648a2f93b859
Mathias Dalheimer <md@gonium.net>
2011-09-18 15:56:15 +0200

    added license header

--- src/sudokusolve.cpp
+++ src/sudokusolve.cpp
@@ -1,6 +1,21 @@
-/*
- * TODO: GPL header
-*
+/**
+ * This file is part of sudoku.
+ *
+ * (c) Mathias Dalheimer <md@gonium.net>, 2011
+ *
+ * sudoku is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * sudoku is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sudoku. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include <gecode/driver.hh>
